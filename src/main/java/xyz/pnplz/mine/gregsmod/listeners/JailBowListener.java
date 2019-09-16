69c37625c12f2d1a3776544065d6cf7b9142eb36
John Pena <john.pena@li.me>
2019-09-15 19:41:56 -0700

    Get jail coordinates from the ENV

--- src/main/java/xyz/pnplz/mine/gregsmod/listeners/JailBowListener.java
+++ src/main/java/xyz/pnplz/mine/gregsmod/listeners/JailBowListener.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,12 +12,14 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class JailBowListener implements Listener {
 
-  private static final String worldName = "Gregbuquerque";
-  private static final Double jailCoordX = 265.0;
-  private static final Double jailCoordY = 65.0;
-  private static final Double jailCoordZ = -31.5;
+  private static final String WORLD_NAME_ENV_VAR = "WORLD_NAME";
+
+  private static final String defaultWorldName = "Gregbuquerque";
+  private static final Double defaultJailCoordX = 265.0;
+  private static final Double defaultJailCoordY = 65.0;
+  private static final Double defaultJailCoordZ = -31.5;
+
   private static final Server server = Bukkit.getServer();
-  private static final Location jail = new Location(server.getWorld(worldName), jailCoordX, jailCoordY, jailCoordZ);
 
   @EventHandler
   public void onPlayerInteract(EntityDamageByEntityEvent event) {
@@ -28,7 +29,49 @@ public class JailBowListener implements Listener {
     if (event.getEntity() instanceof Player && isSpectralArrow) {
       Player player = (Player) event.getEntity();
       Bukkit.getLogger().info("Sending that fucker to jail");
-      player.teleport(jail);
+      player.teleport(getJailLocation());
     }
+  }
+
+  private String getWorldName() {
+    String name = System.getenv(WORLD_NAME_ENV_VAR);
+    if (name.equals("")) {
+      return defaultWorldName;
+    }
+    return name;
+  }
+
+  private Double getCoord(String envVarName, Double defaultVal) {
+    String coordString = System.getenv(envVarName);
+    if (coordString.equals("")) {
+      return defaultVal;
+    }
+
+    try {
+      Double coord = Double.parseDouble(coordString);
+      return coord;
+    } catch (NumberFormatException e) {
+      return defaultVal;
+    }
+  }
+
+  private Double getJailCoordinateX() {
+    return getCoord("JAIL_COORD_X", defaultJailCoordX);
+  }
+
+  private Double getJailCoordinateY() {
+    return getCoord("JAIL_COORD_Y", defaultJailCoordY);
+  }
+
+  private Double getJailCoordinateZ() {
+    return getCoord("JAIL_COORD_Z", defaultJailCoordZ);
+  }
+  private Location getJailLocation() {
+    return new Location(
+        server.getWorld(getWorldName()),
+        getJailCoordinateX(),
+        getJailCoordinateY(),
+        getJailCoordinateZ()
+    );
   }
 }
